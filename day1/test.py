65e89223f5badbf045340a1872a1529fd20ddd4a
QlQkQjsWjrzbxlrnldya <hiyasamo@gmail.com>
2023-01-15 22:39:40 +0900

    연산자

---
+++ day1/test.py
@@ -0,0 +1,31 @@
+# print("아잉잉")
+# i=0
+# print(i)
+'''
+연산자(opeartor)
++ 더하기
+- 빼기
+* 곱하기
+/ 나누기
+** 거듭제곱
+// 몫
+% 나머지
+'''
+# a=20
+# s=10
+# print(a+s)
+# print(a-s)
+# print(a*s)
+# print(a/s)
+
+# print(2**2)
+# print(2**3)
+# print(3**3)
+
+# print(10//3)
+# print(50//2)
+# print(37//4)
+
+# print(10%3)
+# print(50%2)
+# print(37%4)
